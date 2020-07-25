4f7bdd375925a500778e754572eafb81edf2c322
jdawg3636 <jdawg3636minecraft@gmail.com>
2020-07-25 03:20:35 -0500

    Fixed a bug where empty bottles did not drop on death

--- src/main/java/com/jdawg3636/bleachmod/ItemWindexBottle.java
+++ src/main/java/com/jdawg3636/bleachmod/ItemWindexBottle.java
@@ -3,6 +3,7 @@ package com.jdawg3636.bleachmod;
 import com.jdawg3636.bleachmod.core.Reference;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.*;
 import net.minecraft.world.World;
 
@@ -25,13 +26,17 @@ public class ItemWindexBottle extends ItemFood {
         // Call super (handles statistics etc.)
         super.onItemUseFinish(stack, worldIn, entityLiving);
 
-        // Inflict Damage
         if (entityLiving instanceof EntityPlayer) {
-            if (!worldIn.isRemote) entityLiving.attackEntityFrom(Reference.windexDamage, 600.0F);
+            // Cast to Player
+            EntityPlayer entityplayer = (EntityPlayer)entityLiving;
+            // Give Empty Bottle
+            entityplayer.inventory.addItemStackToInventory(new ItemStack(Reference.modItems[2]));
+            // Inflict Damage
+            if (!worldIn.isRemote) entityLiving.attackEntityFrom(Reference.bleachDamage, 600.0F);
         }
 
-        // Return ItemStack of Empty Bottle
-        return new ItemStack(Reference.modItems[2]);
+        // Return Empty ItemStack
+        return new ItemStack(Items.AIR);
 
     }
 
