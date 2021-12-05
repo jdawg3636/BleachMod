4181aeba09575c8c6831d7f6cc176f0ad9052f40
jdawg3636 <jdawg3636minecraft@gmail.com>
2021-12-05 17:41:51 -0600

    Update to 1.18, Overhaul Blocks of Solidified Bleach, Bump Version (1.4)
    https://www.curseforge.com/minecraft/mc-mods/jdawg3636s-bleach-mod/files/3551020
    - Update to 1.18
    - Overhaul Blocks of Solidified Bleach (Now look and behave similar to Slime Blocks)
    - Bump version to 1.4

--- src/main/java/com/jdawg3636/bleachmod/BlockSolidBleach.java
+++ src/main/java/com/jdawg3636/bleachmod/BlockSolidBleach.java
@@ -1,20 +1,24 @@
 package com.jdawg3636.bleachmod;
 
 import com.jdawg3636.bleachmod.core.Reference;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlimeBlock;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.Level;
 
-public class BlockSolidBleach extends Block {
+public class BlockSolidBleach extends SlimeBlock {
 
     public BlockSolidBleach() {
-        super(Block.Properties.of(Material.CAKE).strength(1.0F));
+        super(BlockBehaviour.Properties.of(Material.CLAY, MaterialColor.GRASS).friction(0.8F).sound(SoundType.SLIME_BLOCK).noOcclusion());
         setRegistryName(Reference.MODID, "solid_bleach");
     }
 
@@ -28,6 +32,16 @@ public class BlockSolidBleach extends Block {
     public void playerWillDestroy(Level worldIn, BlockPos pos, BlockState state, Player player) {
         if (!worldIn.isClientSide()) player.hurt(Reference.solidBleachDamage, 600.0F);
         super.playerWillDestroy(worldIn, pos, state, player);
+    }
+
+    @Override
+    public boolean isSlimeBlock(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isStickyBlock(BlockState state) {
+        return true;
     }
 
 }
