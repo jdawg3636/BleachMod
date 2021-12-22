8a3badb834c46b37d848728bcaf028a0a93eed2a
jdawg3636 <jdawg3636minecraft@gmail.com>
2021-12-21 23:53:25 -0600

    Backport 1.3 and 1.4 Updates from MC 1.18 to MC 1.16
    - Fixed Blocks of Solidified Bleach not dropping as items when broken
    - Overhaul Blocks of Solidified Bleach (Now look and behave similar to Slime Blocks)
    - Bump version to 1.4

--- src/main/java/com/jdawg3636/bleachmod/BlockSolidBleach.java
+++ src/main/java/com/jdawg3636/bleachmod/BlockSolidBleach.java
@@ -2,8 +2,11 @@ package com.jdawg3636.bleachmod;
 
 import com.jdawg3636.bleachmod.core.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.block.SlimeBlock;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -11,23 +14,33 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 
-public class BlockSolidBleach extends Block {
+public class BlockSolidBleach extends SlimeBlock {
 
     public BlockSolidBleach() {
-        super(Block.Properties.of(Material.CAKE).strength(1.0F));
+        super(Block.Properties.of(Material.CLAY, MaterialColor.GRASS).friction(0.8F).sound(SoundType.SLIME_BLOCK).noOcclusion());
         setRegistryName(Reference.MODID, "solid_bleach");
     }
 
     @Override
     public ActionResultType use(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        if (!worldIn.isClientSide) player.hurt(Reference.solidBleachDamage, 600.0F);
+        if (!worldIn.isClientSide()) player.hurt(Reference.solidBleachDamage, 600.0F);
         return super.use(state, worldIn, pos, player, handIn, hit);
     }
 
     @Override
     public void playerWillDestroy(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
-        if (!worldIn.isClientSide) player.hurt(Reference.solidBleachDamage, 600.0F);
+        if (!worldIn.isClientSide()) player.hurt(Reference.solidBleachDamage, 600.0F);
         super.playerWillDestroy(worldIn, pos, state, player);
+    }
+
+    @Override
+    public boolean isSlimeBlock(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isStickyBlock(BlockState state) {
+        return true;
     }
 
 }
--- src/main/java/com/jdawg3636/bleachmod/core/BleachMod.java
+++ src/main/java/com/jdawg3636/bleachmod/core/BleachMod.java
@@ -1,6 +1,8 @@
 package com.jdawg3636.bleachmod.core;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
@@ -8,6 +10,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod(Reference.MODID)
 public class BleachMod {
@@ -34,6 +37,15 @@ public class BleachMod {
                 event.getRegistry().register(new BlockItem(Reference.modBlocks[i], new Item.Properties().tab(ItemGroup.TAB_SEARCH)).setRegistryName(Reference.modBlocks[i].getRegistryName()));
             }
         }
+
+        @SubscribeEvent
+        public static void onClientSetupEvent(FMLClientSetupEvent event) {
+            event.enqueueWork(() ->
+                    // Assign RenderType for Block of Solidified Bleach
+                    RenderTypeLookup.setRenderLayer(Reference.modBlocks[0], RenderType.translucent())
+            );
+        }
+
     }
 
 }
